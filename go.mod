de4764c4ad392b2922dbc595ce829ba516c8e3aa
albcui <albcui@nvidia.com>
2024-04-05 05:39:11 +0000

    Secure with TLS

--- go.mod
+++ go.mod
@@ -3,17 +3,19 @@ module github.com/albscui/proglog
 go 1.19
 
 require (
-	github.com/stretchr/testify v1.8.1
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d
+	github.com/stretchr/testify v1.8.4
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231016165738-49dd2c1f3d0b
 	google.golang.org/grpc v1.59.0
-	google.golang.org/protobuf v1.31.0
+	google.golang.org/protobuf v1.32.0
 )
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
 
 require (
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,4 +1,4 @@
-package server_test
+package server
 
 import (
 	"context"
@@ -8,15 +8,15 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 
 	api "github.com/albscui/proglog/api/v1"
+	"github.com/albscui/proglog/internal/config"
 	"github.com/albscui/proglog/internal/log"
-	"github.com/albscui/proglog/internal/server"
 )
 
-type testCase func(*testing.T, api.LogClient, *server.Config)
+type testCase func(*testing.T, api.LogClient, *Config)
 
 // TestServer defines all test cases and runs a subtest for each.
 func TestServer(t *testing.T) {
@@ -34,17 +34,34 @@ func TestServer(t *testing.T) {
 }
 
 // setupTest creates the client and runs the server locally.
-func setupTest(t *testing.T) (client api.LogClient, config *server.Config, teardown func()) {
+func setupTest(t *testing.T) (client api.LogClient, cfg *Config, teardown func()) {
 	t.Helper()
 
 	// Create a listener on the local network address that our server will run on.
 	// The 0 port will automatically assign us a free port.
-	l, err := net.Listen("tcp", ":0")
+	l, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
 
-	// Create an insecure connection to our listener, which will be used by our client.
-	cc, err := grpc.Dial(l.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// Configure our client's TLS credentials to use our CA as the client's Root CA (the CA it will use to verify the server).
+	clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{CAFile: config.CAFile})
 	require.NoError(t, err)
+
+	clientCreds := credentials.NewTLS(clientTLSConfig)
+
+	cc, err := grpc.Dial(l.Addr().String(), grpc.WithTransportCredentials(clientCreds))
+	require.NoError(t, err)
+
+	// Hook up our server with its certificate and enable it to handle TLS connections.
+	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CAFile:        config.CAFile,
+		CertFile:      config.ServerCertFile,
+		KeyFile:       config.ServerKeyFile,
+		ServerAddress: l.Addr().String(),
+		Server:        true,
+	})
+	require.NoError(t, err)
+
+	serverCreds := credentials.NewTLS(serverTLSConfig)
 
 	// Create our server and start serving requests in a goroutine.
 	dir, err := os.MkdirTemp("", "server-test")
@@ -53,8 +70,8 @@ func setupTest(t *testing.T) (client api.LogClient, config *server.Config, teard
 	clog, err := log.NewLog(dir, log.Config{})
 	require.NoError(t, err)
 
-	config = &server.Config{CommitLog: clog}
-	gsrv, err := server.NewGRPCServer(config)
+	cfg = &Config{CommitLog: clog}
+	gsrv, err := NewGRPCServer(cfg, grpc.Creds(serverCreds))
 	require.NoError(t, err)
 
 	// non-blocking
@@ -73,7 +90,7 @@ func setupTest(t *testing.T) (client api.LogClient, config *server.Config, teard
 
 // tests that producing and consuming works by using our client and server to produce a redord to the log,
 // consume it back, and then check the record we sent is the same one we got back.
-func testProduceConsume(t *testing.T, client api.LogClient, _ *server.Config) {
+func testProduceConsume(t *testing.T, client api.LogClient, _ *Config) {
 	ctx := context.Background()
 
 	want := &api.Record{Value: []byte("hello world")}
@@ -88,7 +105,7 @@ func testProduceConsume(t *testing.T, client api.LogClient, _ *server.Config) {
 }
 
 // tests that our server responds with an api.ErrOffsetOutOfRange error when a client tries to consume beyong the log's boundaries.
-func testConsumePastBoundary(t *testing.T, client api.LogClient, _ *server.Config) {
+func testConsumePastBoundary(t *testing.T, client api.LogClient, _ *Config) {
 	ctx := context.Background()
 
 	produce, err := client.Produce(ctx, &api.ProduceRequest{Record: &api.Record{Value: []byte("hello world")}})
@@ -107,7 +124,7 @@ func testConsumePastBoundary(t *testing.T, client api.LogClient, _ *server.Confi
 }
 
 // testProduceConsumeStream tests whether we can produce and consume through stream.
-func testProduceConsumeStream(t *testing.T, client api.LogClient, _ *server.Config) {
+func testProduceConsumeStream(t *testing.T, client api.LogClient, _ *Config) {
 	ctx := context.Background()
 
 	records := []*api.Record{
